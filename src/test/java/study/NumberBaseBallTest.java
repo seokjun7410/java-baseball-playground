a3e9ab395c31dbefea08750dbb3eefaa42ce364b
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-15 16:10:20 +0900

    test: create NumberBaseBallTest.java with setUp()

---
+++ src/test/java/study/NumberBaseBallTest.java
@@ -0,0 +1,12 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+public class NumberBaseBallTest {
+
+    private String answer;
+
+    @BeforeEach
+    void setUp() {
+        answer = "625";
+    }
+}
