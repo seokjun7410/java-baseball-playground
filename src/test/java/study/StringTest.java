cfecad36d5b5e0b99913135802e9e697782ed46b
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-15 15:02:13 +0900

    test: split()

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -10,4 +11,13 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String str = "1,2";
+        String[] split = str.split(",");
+        assertThat(split[0]).isEqualTo("1");
+        assertThat(split[1]).isEqualTo("2");
+    }
+
 }
