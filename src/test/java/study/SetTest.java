2e3bc27e41e930b10e0971e91feafaf2e6ca5daf
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-15 15:17:54 +0900

    test: setTest.containsV2() with @CsvSource

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -23,13 +24,20 @@ public class SetTest {
     }
 
     @Test
-    void size(){
+    void size() {
         assertThat(numbers.size()).isEqualTo(3);
     }
 
     @ParameterizedTest
-    @ValueSource(ints ={1,2,3})
-    void contains(int number){
+    @ValueSource(ints = {1, 2, 3})
+    void contains(int number) {
         assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void containsV2(int input, String expectedString) {
+        boolean expected = Boolean.parseBoolean(expectedString);
+        assertThat(numbers.contains(input)).isEqualTo(expected);
     }
 }
