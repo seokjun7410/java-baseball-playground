5d6cf8c5aecd8e48fb56789da3ded0d1a6d11e88
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2024-01-15 15:34:49 +0900

    test: create StringCalculatorTest.java with setUp()

---
+++ src/test/java/study/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class StringCalculatorTest {
+
+    String input;
+    @BeforeEach
+    void setUp() {
+        input = "2 + 3 * 4 / 2";
+    }
+}
